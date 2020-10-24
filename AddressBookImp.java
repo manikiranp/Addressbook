26ca3502b62c0219bb29f3bfc66c9d87ba98d0cc
manikiranp <manikiranp3@gmail.com>
2020-10-24 20:04:53 +0530

    deletePerson method included in AddressbookImp

--- AddressBookImp.java
+++ AddressBookImp.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 class AddressBookImp implements AddressBookInterface {
-	public static ArrayList<Person> addressbook;
+	private static final String String = null;
+	public  ArrayList<Person> addressbook;
+
 	public AddressBookImp() {
 		addressbook = new ArrayList<Person>();
 	}
 	
 	public void addPerson() {
-		
+	
 		Scanner inp = new Scanner(System.in);
 		System.out.println("Enter the firstName:");
 		String firstName = inp.nextLine();
@@ -30,7 +32,7 @@ class AddressBookImp implements AddressBookInterface {
 
 	}
 
-	@Override
+	
 	public void editPerson() {
 		Scanner n = new Scanner(System.in);
 		System.out.println("Enter firstname to update details");
@@ -43,7 +45,7 @@ class AddressBookImp implements AddressBookInterface {
 				addressbook.remove(i);
 				System.out.println("Enter new number:");
 				String num = n.nextLine();
-				Person p = new Person(num);
+				Person p = new Person(num, num, num, num, num, num);
 				addressbook.add(p);
 			}
 		}
@@ -52,9 +54,23 @@ class AddressBookImp implements AddressBookInterface {
 			}
 	}
 
-	@Override
+	
 	public void deletePerson() {
-		// TODO Auto-generated method stub
+		Scanner n = new Scanner(System.in);
+		System.out.println("Enter firstname to delete its entries");
+		String name = n.nextLine();
+		boolean isfound = false;
+		for (int i=0; i<addressbook.size(); i++) {
+			String personName = addressbook.get(i).firstname;
+			if (name.equals(personName)) {
+				isfound = true;
+				addressbook.remove(i);
+				System.out.println("Entry deleted");
+			}
+		}
+			if (isfound == false) {
+				System.out.println("Not found");
+			}
 		
 	}
 
